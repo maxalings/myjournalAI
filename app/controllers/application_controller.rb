996e8187927d4539a30de9af325282e9ab211d27
Daniel Rodriguez <daniel.rod93@gmail.com>
2024-07-30 15:41:52 +0200

    Finished the devise setuo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = current_user.try(:locale) || I18n.default_locale
+  end
 end
