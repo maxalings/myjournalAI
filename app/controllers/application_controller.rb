3d8e87574eb4d68bce6b1d250bcc0dc1a234f2f0
Daniel Rodriguez <daniel.rod93@gmail.com>
2024-08-20 10:11:29 +0200

    added meta tags setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = current_user.try(:locale) || I18n.default_locale
   end
+
+  # This method overrides Rails.application.default_url_options[:host] to add an absolute URL to meta tags, good for SEO
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
