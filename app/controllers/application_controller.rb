92e157bbdaf6ba0334b7ccaf0c7dc873ffc49457
max alings <alings.max@gmail.com>
2024-12-12 14:55:49 +0800

    front end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,9 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
+
+  def after_sign_in_path_for(resource)
+    journals_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'checkout', to: 'checkouts#show'
   get 'checkout/success', to: 'checkouts#success'
-  root to: "journals#index"
+  root to: "pages#home"
   get 'terms-and-conditions', to: 'pages#terms'
   get 'privacy-policy', to: 'pages#privacy'
   get 'cookies', to: 'pages#cookies'
